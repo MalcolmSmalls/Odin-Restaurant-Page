7eabf95d9c549172e9b95b5c4c6301c70672a81b
MalcolmSmalls <smallsbeats@gmail.com>
2022-09-24 14:45:53 -0700

    Add connection between js and menu tab

---
+++ src/menu.js
@@ -0,0 +1,22 @@
+const menu = () => {
+    const content = document.getElementById('content')
+
+    content.innerHTML = ""
+
+    const h1 = document.createElement('h1')
+
+    content.appendChild(h1).textContent = "Bean's Burritos Menu"
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export { menu }
--- src/pageload.js
+++ src/pageload.js
@@ -1,8 +1,29 @@
+import { menu } from "./menu.js"
+
 // const pageLoad = () => {
 
 const content = document.getElementById('content')
 
 const h1 = document.createElement('h1')
+
+const header = document.createElement('header')
+
+
+
+header.innerHTML = `
+    <nav>
+        <ul>
+            <li class="home">Home</li>
+            <li class="menu">Menu</li>
+            <li class="contact">Contact</li>
+        </ul>
+    </nav>
+`
+content.appendChild(header)
+
+const getMenu = document.querySelector('.menu')
+
+getMenu.addEventListener('click', menu)
 
 let img = document.createElement('img')
 img.src = "../img/burrito.jpeg"
@@ -15,10 +36,11 @@ p.textContent = "The finest vegan burritos in all the land."
 
 content.appendChild(img)
 
+content.appendChild(h1).textContent = "Bean's Burritos"
+
 content.appendChild(p)
 
 
-content.appendChild(h1).textContent = "Bean's Burritos"
     
 
 
